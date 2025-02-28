ba1fe65f9d34c9795e83356e38e21ff3a726c29b
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2025-02-28 18:11:54 -0500

    fine shi rl ts pmo

---
+++ optimizer/brute/bruteparamsearch.py
@@ -0,0 +1,10 @@
+# brute search for hyperparam space:
+
+# [c1] X [c2] X [c3] X [c4] :: => results in surveying partition of R4 space
+
+# not optimal or reccomended . use bayesian
+
+
+from tinygrad import Tensor, nn
+
+
