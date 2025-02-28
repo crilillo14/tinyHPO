b0b101b57472a2dbe1d6a8c92fd776b5096db279
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2025-02-28 18:15:43 -0500

    lwk ts promising

---
+++ optimizer/hyperparamOptim.py
@@ -0,0 +1,13 @@
+# imports 
+
+
+from typing import List, Callable
+from tinygrad import Tensor, TinyJit, nn, GlobalCounters
+from tinygrad.helpers import getenv, colored, trange
+
+
+
+class hyperparameterOptimizer : 
+    def __init__(self,
+                 model : ):
+        
