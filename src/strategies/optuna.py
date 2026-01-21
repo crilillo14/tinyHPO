c99837fa5211b2b133687a918288e520ec74cf03
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2026-01-21 14:49:36 -0500

    changes design direction

--- src/strategies/optuna.py
+++
@@ -1,4 +0,0 @@
-import optuna
-# objective function optimization by 3rd party service
-
-study = optuna.create_study()
--- src/typedef.py
+++
@@ -1,7 +0,0 @@
-from typing import dict, tuple, text, union, optional
-
-# complex type definitions go here. static types only. 
-
-class hpo_obj:
-    def __init__(self):
-        pass
